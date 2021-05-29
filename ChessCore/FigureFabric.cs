b3fd2dd4b83b60ab3abde7c17b20087221cb753b
Koi Den <prostobydet@gmail.com>
2021-05-29 20:36:56 +0300

    Make ChessCore

--- ChessCore/FigureFabric.cs
+++ ChessCore/FigureFabric.cs
@@ -8,6 +8,35 @@ namespace ChessCore
 {
     static public class FigureFabric
     {
-        //code 
+        public static Figure Make(string name ,int x1, int y1)
+        {
+            Figure f = null;
+
+            switch(name)
+            {
+                case "":
+                    break;
+                case "King":
+                    f = new King(x1, y1);
+                    break;
+                case "Queen":
+                    f = new Queen(x1, y1);
+                    break;
+                case "Bishop":
+                    f = new Bishop(x1, y1);
+                    break;
+                case "Knight":
+                    f = new Knight(x1, y1);
+                    break;
+                case "Rook":
+                    f = new Rook(x1, y1);
+                    break;
+                case "Pawn":
+                    f = new Pawn(x1, y1);
+                    break;
+            }
+
+            return f;
+        }
     }
 }
--- Сhess/Program.cs
+++ Сhess/Program.cs
@@ -11,35 +11,33 @@ namespace Сhess
         static void Main(string[] args)
         {
             string choose;
+            Figure king = null;
 
             while (true)
             {
                 Console.WriteLine("Menu:");
-                Console.WriteLine("     King's   Move  - K");
-                Console.WriteLine("     Queen's  Move  - Q");
-                Console.WriteLine("     Bishop's Move  - B");
-                Console.WriteLine("     Knight's Move  - KN");
-                Console.WriteLine("     Rook's   Move  - R");
-                Console.WriteLine("     Pawn's   Move  - P");
-                Console.WriteLine("     Exit           - X");
+                Console.WriteLine("  Create King - K");
+                Console.WriteLine("  Try Move    - M x1 y1");
+                Console.WriteLine("  Exit        - X");
                 choose = Console.ReadLine().ToUpper();
-                switch (choose)
+                string[] subs = choose.Split(' ');
+                switch (subs[0])
                 {
-                    case "K":  break;
-                    case "Q": break;
-                    case "B": break;
-                    case "KN": break;
-                    case "R": break;
-                    case "P": break;
+                    case "K": 
+                        king = FigureFabric.Make("King", 1, 1);
+                        Console.WriteLine("The king is created. Make a move");
+                        break;
+                    case "M":
+                        try {
+                            Console.WriteLine(king.Move(int.Parse(subs[1]), int.Parse(subs[2])));
+                        }
+                        catch {
+                            //ignore
+                        }
+                        break;
                     case "X": return;
                 }
             }
         }
-
-        //static public void AddKing()
-        //{
-        //    King k = new King(1, 1);
-        //    figures.Add(k);
-        //}
     }
 }
